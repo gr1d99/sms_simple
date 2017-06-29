b59248adfa51b6054ffef696877de35cbeb82b0d
gr1d99 <gideonkimutai9@gmail.com>
2017-06-30 00:42:21 +0300

    created Main Configuration Model add create Africas Talking Model

--- models.py
+++ models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class MainConfiguration(models.Model):
+    api_key = models.CharField(max_length=255, blank=True)
+    secret_key = models.CharField(max_length=255, blank=True)
+
+
+class AfricasTalking(MainConfiguration):
+    username = models.CharField(max_length=255)
