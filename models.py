61eadcf27d5ecb0661b948135f346ac29be8bfee
gr1d99 <gideonkimutai9@gmail.com>
2017-07-31 16:35:50 +0300

    updated existing model to proxy

--- models.py
+++ models.py
@@ -4,17 +4,23 @@ from __future__ import unicode_literals
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
+
 class MainConfiguration(models.Model):
+
     username = models.CharField(max_length=255, blank=True, unique=True)
     api_key = models.CharField(max_length=255, blank=True)
     secret_key = models.CharField(max_length=255, blank=True)
 
+    class Meta:
+        abstract = True
+
 
 class AfricasTalking(MainConfiguration):
-    class Meta:
-        verbose_name_plural = "Africas Talking"
 
     default = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Africas Talking"
 
 
 class AfricasTalkingTestReceivers(models.Model):
